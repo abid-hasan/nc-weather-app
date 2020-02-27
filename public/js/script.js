534decea1b85b2b41e85c6308c445505719805aa
Abid-Hasan <ahsnigdho@gmail.com>
2020-02-28 00:20:36 +0600

    Update help page

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,7 @@ console.log("Client side JavaScript file is loaded.");
 
 const weatherForm = document.querySelector("form");
 const input = document.querySelector("input");
+const checkHelp = document.querySelector("#checkHelp");
 const messageOne = document.querySelector("#messageOne");
 const messageTwo = document.querySelector("#messageTwo");
 
@@ -11,14 +12,18 @@ weatherForm.addEventListener("submit", (e) => {
   const location = input.value;
   const url = "/weather?address=" + location;
 
+  checkHelp.textContent = "";
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
 
   fetch(url).then((response) => {
     response.json().then((data) => {
       if (data.error) {
-        return messageOne.textContent = data.error;
+        messageOne.textContent = data.error;
+        messageTwo.innerHTML = 'Need help? <a href="/help">Click here</a>.';
+        return;
       }
+      checkHelp.innerHTML = 'Location not found? <a href="/help">Check help</a>.';
       messageOne.textContent = data.location;
       messageTwo.textContent = data.forecast;
     });

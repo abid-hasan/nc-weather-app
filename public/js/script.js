6857763b183ae5429e8de1d350060e3cbdde4d06
Abid-Hasan <ahsnigdho@gmail.com>
2020-02-27 15:10:50 +0600

    Setup app for Heroku

--- public/js/script.js
+++ public/js/script.js
@@ -9,7 +9,7 @@ weatherForm.addEventListener("submit", (e) => {
   e.preventDefault(); // To prevent page from reloading on submission
 
   const location = input.value;
-  const url = "http://localhost:3000/weather?address=" + location;
+  const url = "/weather?address=" + location;
 
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
